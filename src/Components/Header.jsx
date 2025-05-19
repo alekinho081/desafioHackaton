c41ded400f1dd347b55790d18a0f0eb9be64c880
Alekinho <alexborbajr0@gmail.com>
2025-05-19 15:00:10 -0300

    estilização melhorada

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import styles from "../Css/Header.module.css";
+import { NavLink } from "react-router-dom"; // Se estiver usando React Router
+import Avatars from "./Avatar";
 
 export const Header = () => {
-    return(
-        <div style={{backgroundColor:'aquamarine'}}>
-            <nav style={{justifyContent:'space-around', display:'flex', height:'30px'}}>
-                <a style={{textDecoration:'none'}} href="/livros">Cadastre livros</a>
-                <a style={{textDecoration:'none'}} href="/reserva">Reserve livros</a>
-                <a style={{textDecoration:'none'}} href="/">Home</a>
-            </nav>
-
-
-        </div>
-    )
-
-}
-
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <NavLink 
+          to="/livros" 
+          className={({ isActive }) => 
+            isActive ? `${styles.navLink} ${styles.activeLink}` : styles.navLink
+          }
+        >
+          Cadastre livros
+        </NavLink>
+        <NavLink 
+          to="/reserva" 
+          className={({ isActive }) => 
+            isActive ? `${styles.navLink} ${styles.activeLink}` : styles.navLink
+          }
+        >
+          Reserve livros
+        </NavLink>
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => 
+            isActive ? `${styles.navLink} ${styles.activeLink}` : styles.navLink
+          }
+        >
+          Home
+        </NavLink>
+      </nav>
+      <div>
+        <Avatars nome={'Alex'}/>
+      </div>
+    </header>
+  );
+};
